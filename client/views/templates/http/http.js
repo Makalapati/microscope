5c1df62f8331702a3e5cb2fd2d6d727987d94261
makalapati <dhomou@live.fr>
2016-05-25 03:16:41 +0200

    EventBrite - pull events from API

--- client/views/templates/http/http.js
+++ client/views/templates/http/http.js
@@ -12,8 +12,8 @@
 //         console.log(response);
 //     }
 // });
-var EVENTBRITE_ACCESS_TOKEN = 'GRMKL6M535BLYEEF72RE';
-var ORGANIZER_ID = '10618084925';
+// var EVENTBRITE_ACCESS_TOKEN = 'GRMKL6M535BLYEEF72RE';
+// var ORGANIZER_ID = '10618084925';
 // var Nbrite = require('nbrite');
 // var nbrite = new Nbrite(EVENTBRITE_ACCESS_TOKEN);
 
@@ -59,17 +59,55 @@ var ORGANIZER_ID = '10618084925';
 //     }
 // });
 
-if(Meteor.isServer) {
-    Meteor.methods({
-        eventBrite: function() {
-            this.unblock();
-            return Meteor.http.call('https://www.eventbrite.com/json/events_search?token='+EventbriteAPI+'&organizer.id='+ORGANIZER_ID);
-        }
-    });
-}
+// if(Meteor.isServer) {
+//     Meteor.methods({
+//         eventBrite: function() {
+//             this.unblock();
+//             return Meteor.http.call("GET", "https://www.eventbrite.com/json/events_search/?token=GRMKL6M535BLYEEF72RE&organizer.id=10618084925");
+//         }
+//     });
+// }
 
-if(Meteor.isClient) {
-    Meteor.call('eventbrite',function (error, result) {
-        console.log(results.content);
-    });
-}
+// if(Meteor.isClient) {
+//     Meteor.call('eventbrite',function (error, result) {
+//         console.log(results.content);
+//     });
+// }
+
+// if (Meteor.isServer) {
+//     Meteor.methods({
+//         checkTwitter: function () {
+//             this.unblock();
+//             return Meteor.http.call("GET", "https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=734717590686334976&screen_name=BlackStaar0");
+//         }
+//     });
+// }
+
+// //invoke the server method
+// if (Meteor.isClient) {
+//     Meteor.call("checkTwitter", function(error, results) {
+//         console.log(results.content); //results.data should be a JSON object
+//     });
+// }
+
+// Meteor.http.call("GET", "https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=734717590686334976&screen_name=BlackStaar0",
+//     {}, function(err, response){
+//         console.log(response.content);
+//     });
+Template.http.helpers({
+    flux : function(){
+        Meteor.http.get('https://www.eventbriteapi.com/v3/events/search/?token=GRMKL6M535BLYEEF72RE&organizer.id=10618084925&format=json',
+        {
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization': 'Bearer GRMKL6M535BLYEEF72RE'
+            }
+        }, function(error, response){
+                if(error){
+                    console.log(error);
+                } else { 
+                    console.log(response.content);
+                }
+            })
+    }
+});
